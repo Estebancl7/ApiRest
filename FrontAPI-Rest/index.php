4b9d980484199836edbec2d2234bc743f779d632
Esteban Tudela <esteban.tudelao@utem.cl>
2021-06-06 18:28:06 -0400

    Se optimzan detalles y se agrega script.bat

--- FrontAPI-Rest/index.php
+++ FrontAPI-Rest/index.php
@@ -22,7 +22,7 @@
         <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Nosotros</a>
+        <a class="nav-link" href="nosotros.php">Nosotros</a>
       </li>
       
       
---
+++ FrontAPI-Rest/nosotros.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/ClientePhp/src/css/index.css">
+    <!--Font awesome-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+   
+    <title>API REST</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info sticky-top p-3 aria-label">
+        <div class="container">
+            <a class="navbar-brand" href="/ClientePhp/index.php" style="color: black;">Servicio API REST</a>
+            <button class="navbar-toggler border-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="/ClientePhp/index.php" style="text-align: center;">Inicio </a>
+                    </li>
+                    <a href="nombre.php" class="btn btn-outline-light px-2" style="text-align: center; max-width: 850px;">Nombre Completo</a>
+                    <a href="rut.php" class="btn btn-outline-light px-2" style="text-align: center; max-width: 850px;">Rut Verificador</a>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    <section id="first">
+        <div class="container text-center bg-info rounded shadow my-4 p-3 mt-5">
+            <h1 class="divider pt-5 mx-5"></h1>
+            <h1 class="pb-1">
+                <span class="">I</span>
+                <span class="">n</span>
+                <span class="">t</span>
+                <span class="">e</span>
+                <span class="">g</span>
+                <span class="">r</span>
+                <span class="">a</span>
+                <span class="">n</span>
+                <span class="">t</span>
+                <span class="">e</span>
+                <span class="">s</span>
+
+                <span class="ml-3">T</span>
+                <span class="">r</span>
+                <span class="">a</span>
+                <span class="">b</span>
+                <span class="">a</span>
+                <span class="">j</span>
+                <span class="">o</span>
+
+                <span class="ml-3">U</span>
+                <span class="">n</span>
+                <span class="">i</span>
+                <span class="">d</span>
+                <span class="">a</span>
+                <span class="">d</span>
+                <span class="ml-3">1</span>
+            </h1>
+            <h1 class="divider pt-1 mx-5"></h1>
+            <br>
+            <div class="row">
+                <div class="col-6 pl-5">
+                    <P>Este ser&aacute el grupo de trabajo que estar&aacute a cargo del desarrollo de los proyectos a realizar durante el semestre acÃ¡demico del aÃ±o 2021 de la asignatura <strong>"Redes y Comunicaci&oacuten de datos"</strong>.
+                    </P>
+                    <img src="img/Grafos.png" alt="" height="180px">
+                </div>
+                <div class="col-6 pt-1 pr-5">
+                    
+                    <p><em class="fas fa-check-circle"></em> Diego Salinas Gonzalez.</p>
+                    <p><em class="fas fa-check-circle"></em> Esteban Tudela Ortiz.</p>
+                    <p> <em class="fas fa-check-circle"></em> Paulo Vera Chacon.</p>
+                    <p> <em class="fas fa-check-circle"></em> Felipe Perez.</p>
+                    <p><em class="fas fa-check-circle"></em> Javier Cisternas.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="footer">
+        <nav class="container-fluid bg-info fixed-bottom p-2 mt-1 aria-label">
+            <div class="container">
+                <div class="content-center">
+                    <div class="row my-1">
+                        <div class="col d-flex justify-content-center">
+                            <a href="" class="text-white">
+                                <em class="fab fa-github"></em>
+                            </a>
+                        </div>
+                    </div>
+                    <h1 class="divider mx-5"></h1>
+                    <h4 class="text-center"> Universidad Tecnologica Metropolitana | Desarrollado por: Grupo 3</h4>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+   
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/c8152ea011.js" crossorigin="anonymous"></script>
+
+</html>
